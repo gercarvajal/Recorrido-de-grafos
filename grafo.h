9191035d83d23a586284397185e0207eebde3f0d
gercarvajal <gercarvajal@users.noreply.github.com>
2015-09-08 09:29:57 -0300

    Update grafo.h

--- grafo.h
+++ grafo.h
@@ -1,1 +1,19 @@
 // crear la estructura del grafo
+#ifndef STDIO_H
+#include <stdio.h>
+#define STDIO_H
+#endif
+
+#ifndef STDLIB_H
+#include <stdlib.h>
+#define STDLIB_H
+#endif
+
+int vector[1]=1;
+int vector[2]=2;
+int vector[3]=3;
+int vector[4]=4;
+int vector[5]=5;
+int vector[6]=6;
+int vector[7]=7;
+int vector[8]=8;
