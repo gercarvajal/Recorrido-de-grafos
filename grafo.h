1cf8957348130abb16c1c0e8393ac180e4a60ecf
gercarvajal <gercarvajal@users.noreply.github.com>
2015-09-08 13:40:11 -0300

    Update grafo.h

--- grafo.h
+++ grafo.h
@@ -10,10 +10,33 @@
 #endif
 
 int vector[1]=1;
-int vector[2]=2;
-int vector[3]=3;
-int vector[4]=4;
-int vector[5]=5;
-int vector[6]=6;
-int vector[7]=7;
-int vector[8]=8;
+
+
+
+int i=0,j=0;
+for(i=0;i<8;i++){
+	for(j=0;j<8;j++){
+	matriz[i][j]=0;	
+	}
+}
+ 
+matriz[0][1]=1;
+matriz[0][3]=1;
+matriz[1][2]=1;
+matriz[1][4]=1;
+matriz[2][6]=1;
+matriz[3][7]=1;
+matriz[4][0]=1;
+matriz[4][5]=1;
+matriz[5][1]=1;
+matriz[6][5]=1;
+matriz[6][7]=1;
+matriz[7][4]=1;
+
+
+for(i=0;i<8;i++){
+	for(j=0;j<8;j++){
+	printf("%d  ",matriz[i][j]);	
+	}
+printf("\n");
+}
