15fb2ea149601e10a8743a519f93a44b49e22e63
gercarvajal <gercarvajal@users.noreply.github.com>
2015-09-12 14:29:28 -0300

    Update bfs.h

--- bfs.h
+++ bfs.h
@@ -1,1 +1,19 @@
+#ifndef stdio_h 
+#include <stdio.h> 
+#define stdio_h 
+#endif 
+ 
+#ifndef STDLIB_H 
+#include <stdlib.h> 
+#define STDLIB_H 
+#endif 
+
+#ifndef colaB_h
+#include <colaB.h>
+#define colaB_h
+#endif
+
+Cola *creaCola();
+
+
 
