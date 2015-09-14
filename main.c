c345f9be2db21ef7a849c92ff9f444dc215e4b96
gercarvajal <gercarvajal@users.noreply.github.com>
2015-09-13 22:33:10 -0300

    Update main.c

--- main.c
+++ main.c
@@ -10,8 +10,16 @@
 
 main()
 {
-  
-  
-  
+  int opc;
+  printf ("seleccione 1 si quiere proceder con bfs o 2 para dfs");
+  scanf("%d",&opc);
+  if(opc==1)
+  {
+  bfs();  
+    
+  }
+  if(opc==2){
+  dfs();
+  }
   
 }
